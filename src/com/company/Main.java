3cb07f9a32ca4934034a5f8de9aa51fad4d8f065
kutman00 <kutmankalmurzaev45@gmail.com>
2022-03-05 20:56:30 +0600

    added an extra task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,7 +23,32 @@ public class Main {
             }
         }
         System.out.println(result / result2);
-    }
 
+
+        int[] array = {8, -2, -4, 2, 3, 6, -5, 45, -9, 10, 11, -12, -13, 14, 15};
+        System.out.println(Arrays.toString(number(array)));
+
+    } public static int [] number (int[] number1) {
+        for (int i = 0; i < number1.length; i++) {
+            int min = number1[i];
+            int min1 = i;
+            for (int j = i + 1; j < number1.length; j++) {
+                if (number1[j] < min) {
+                    min = number1[j];
+                    min1 = j;
+                }
+            } if (i!= min1 ){
+                int number2 = number1[i];
+                number1 [i] = number1[min1];
+                number1[min1] = number2;
+            }
+        } return number1;
+    }
 }
+
+
+
+
+
+
 
